c5c3bf2408e0bf7b4812f8fcba1b5a89be577dcd
Nataniel Mendes <natanielmendes@hotmail.com>
2021-07-31 16:52:51 -0300

    Setting up a firebase query to sucessfully retrieve data

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const http = require('http');
-require('dotenv').config();
-
+// Express configuration
 const express = require('express');
-
 const app = express();
 
+require('dotenv').config();
+
+import { firebase } from '@firebase/app';
+import '@firebase/database';
+
+// Setting firebase configuration
 const firebaseConfig = {
     apiKey: process.env.apiKey,
     authDomain: process.env.authDomain,
@@ -15,6 +22,10 @@ const firebaseConfig = {
     appId: process.env.appId,
     measurementId: process.env.measurementId
 };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database()
+
+export { db }
 
 app.use((req, res, next) => {
     console.log('Setting up firebase connection middleware');
@@ -22,7 +33,13 @@ app.use((req, res, next) => {
 });
 
 app.get('/recommend', (req, res, next) => {
-    res.send({apiKey: process.env.apiKey, authDomain: process.env.authDomain});
+    let results = [];
+    const threads = firebase.database().ref();
+    const query = threads.limitToFirst(2);
+    query.on('value', snap => {
+        console.log(snap.length);
+        res.send(snap.val());
+    })
 });
 
 const server = http.createServer(app);
