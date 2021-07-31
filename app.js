6503f9c07528398996a159932f865a2bac24c907
Nataniel Mendes <natanielmendes@hotmail.com>
2021-07-31 14:10:42 -0300

    Adding middleware and /recommend route in app.js folder

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Setting up firebase connection middleware');
+    next();
+});
+
+app.get('/recommend', (req, res, next) => {
+    res.send({product: 'product name'});
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
