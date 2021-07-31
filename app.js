1cdca5f83d09de61069fa598261757ceba02e4ee
Nataniel Mendes <natanielmendes@hotmail.com>
2021-07-31 15:10:03 -0300

    Adding firebase configuration as environment variables for security reasons

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http = require('http');
+require('dotenv').config();
 
 const express = require('express');
 
 const app = express();
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
 
 app.use((req, res, next) => {
     console.log('Setting up firebase connection middleware');
@@ -10,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/recommend', (req, res, next) => {
-    res.send({product: 'product name'});
+    res.send({apiKey: process.env.apiKey, authDomain: process.env.authDomain});
 });
 
 const server = http.createServer(app);
